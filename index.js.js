e42a61a89124e6732761c92ad547b56dd5a27933
Matheus Aquino dos Santos <113396919+aquinodev2022@users.noreply.github.com>
2024-03-03 20:27:12 -0300

    Update index.js.js

--- index.js.js
+++ index.js.js
@@ -1,10 +1,13 @@
+// Criação da classe herói
 class Heroi {
+    // Construtor da classe herói que recebe nome, idade e tipo como parâmetros
     constructor(nome, idade, tipo) {
       this.nome = nome;
       this.idade = idade;
       this.tipo = tipo;
     }
 
+    // Método atacar que imprime uma mensagem de ataque com base no tipo de herói
     atacar() {
       let ataque;
   
@@ -24,16 +27,19 @@ class Heroi {
         default:
           ataque = 'usou um ataque desconhecido';
       }
-  
+
+        // Imprime a mensagem de ataque
       console.log(`O ${this.tipo} ${ataque}`);
     }
   }
-  
+
+// Criação dos objetos de cada tipo de herói
   const mago = new Heroi('Merlin', 60, 'mago');
   const guerreiro = new Heroi('Barbáro', 35, 'guerreiro');
   const monge = new Heroi('Rock Lee', 45, 'monge');
   const ninja = new Heroi('Naruto', 30, 'ninja');
-  
+
+// Chamadas dos métodos de ataque para cada herói
   mago.atacar();
   guerreiro.atacar();
   monge.atacar();
